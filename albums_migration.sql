8ee86ed51263bfeede3bf1fce5061cb3b1e68929
Brenden <brenden.m.ferguson@gmail.com>
2023-07-11 14:30:11 -0500

    Completed albums_migration exercise

---
+++ albums_migration.sql
@@ -0,0 +1,11 @@
+USE codeup_test_db;
+drop table if exists albums;
+CREATE TABLE albums (
+                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+                        artist VARCHAR(50),
+                        name  VARCHAR(100) NOT NULL,
+                        release_date INT NOT NULL,
+                        sales FLOAT NOT NULL,
+                        genre VARCHAR(50),
+                        PRIMARY KEY (id)
+);
