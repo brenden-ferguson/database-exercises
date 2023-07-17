c441316733485a045c27c4f153fa4fb395fc81d1
Brenden <brenden.m.ferguson@gmail.com>
2023-07-17 12:30:59 -0500

    Completed subqueries exercise

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -5,3 +5,16 @@ FROM employees
 WHERE hire_date IN (SELECT hire_date
                     FROM employees
                     WHERE emp_no = '101010');
+
+SELECT title
+FROM titles
+WHERE emp_no IN (SELECT emp_no
+                 FROM employees
+                 WHERE first_name = 'Aamod' );
+
+SELECT CONCAT(first_name, ' ', last_name) AS 'full name'
+FROM employees
+WHERE emp_no IN (SELECT emp_no
+                 FROM dept_manager
+                 WHERE gender = 'F'
+                 AND to_date = '9999-01-01');
