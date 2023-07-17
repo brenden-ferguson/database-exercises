a69c83ca475e730360aac827e082935cbab4fb38
Brenden <brenden.m.ferguson@gmail.com>
2023-07-17 16:11:09 -0500

    Refactored

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -1,20 +1,19 @@
 use employees;
 
 SELECT CONCAT(first_name, ' ', last_name) AS full_name
-FROM employees
-WHERE hire_date IN (SELECT hire_date
+FROM employees e
+WHERE e.hire_date IN (SELECT hire_date
                     FROM employees
                     WHERE emp_no = '101010');
 
 SELECT title
-FROM titles
-WHERE emp_no IN (SELECT emp_no
+FROM titles t
+WHERE t.emp_no IN (SELECT emp_no
                  FROM employees
                  WHERE first_name = 'Aamod' );
 
 SELECT CONCAT(first_name, ' ', last_name) AS 'full name'
-FROM employees
-WHERE emp_no IN (SELECT emp_no
+FROM employees e
+WHERE gender = 'F' AND e.emp_no IN (SELECT emp_no
                  FROM dept_manager
-                 WHERE gender = 'F'
-                 AND to_date = '9999-01-01');
+                 WHERE to_date = '9999-01-01');
